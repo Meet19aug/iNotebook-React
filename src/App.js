345cb8223dd4b01aa83818fe31c37de185d9063b
Meet Patel <meetpatel19aug@gmail.com>
2021-12-18 00:19:53 +0530

    Completing Basic React Routing and Bootstrap setup.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Navbar/>
+    <Switch>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/About" element={<About/>} />
+    </Switch>
+      
+      </Router>
+      
+    </>
   );
 }
 
