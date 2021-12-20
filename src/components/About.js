5f17e3b23b1922e0d1bd29816feabd21a8ef0393
Meet Patel <meetpatel19aug@gmail.com>
2021-12-20 17:21:32 +0530

    Making about us page more information and clean.

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,46 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            <h1>This is about page</h1>
+            <div className="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+                <div className="my-3 py-3">
+                    <h2 className="display-5">iNotebook</h2>
+                    <p className="lead">Your Notes is secure in Cloud.</p>
+                </div>
+                <div className="bg-light box-shadow mx-auto" style={{ width: "80%", height: "260px", borderRadius: "21px 21px 0 0", color: "black" }}>
+                    <div className="accordion accordion-flush" id="accordionFlushExample">
+                        <div className="accordion-item">
+                            <h2 className="accordion-header" id="flush-headingOne">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                    Save your notes with iNotebook
+                                </button>
+                            </h2>
+                            <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                <div className="accordion-body">INotebook is personalized notes saver on cloud, You can create a new account, Login with your credentials and ready to save your notes with iNotebook. It is simple interface with clean UI. Just save note once and access your note from anywhere in the world.</div>
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <h2 className="accordion-header" id="flush-headingTwo">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                Free to use
+                                </button>
+                            </h2>
+                            <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                                <div className="accordion-body">With iNotebook you can save your notes in cloud for free, We are not charging any single panny for that, we are glad to see for using our world class plateform. </div>
+                            </div>
+                        </div>
+                        <div className="accordion-item">
+                            <h2 className="accordion-header" id="flush-headingThree">
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                                    Browser Compatiblity
+                                </button>
+                            </h2>
+                            <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                                <div className="accordion-body">This iNotebook software works in any web browsers such as Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to save your important notes on cloud with our best security. </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
